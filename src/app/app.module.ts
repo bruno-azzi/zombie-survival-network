22b5b5bbbf8a17c21ae75713b9b29234e3e4676c
Bruno Lessa Azzi <bruno.azzi@intranet.rca.net>
2019-03-24 22:58:54 -0300

    Initial commit with bootstrap

---
+++ src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { AppRoutingModule } from './app-routing.module';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+import { DashboardModule } from './dashboard/dashboard.module';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    DashboardModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
